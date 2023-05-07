e85ae8db95cde07ed4c23cc2806fba875f399576
Rafael Oliveira <102834960+farastyle@users.noreply.github.com>
2023-05-07 04:53:43 -0300

    Update homologa.py

--- homologa.py
+++ homologa.py
@@ -14,18 +14,18 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
 
 
 async def create_button_row(activities):
-    buttons_row1 = [Button(style=ButtonStyle.green, custom_id="logar_button")]
+    buttons_row1 = [Button(button_style=ButtonStyle.green, custom_id="logar_button")]
     row1 = activities[0:5]
     row2 = activities[5:10]
 
     for activity in row1:
-        button = Button(style=ButtonStyle.blue, label=activity, custom_id=activity.lower().replace(" ", "_"))
+        button = Button(button_style=ButtonStyle.blue, label=activity, custom_id=activity.lower().replace(" ", "_"))
         buttons_row1.append(button)
 
     buttons_row2 = []
 
     for activity in row2:
-        button = Button(style=ButtonStyle.blue, label=activity, custom_id=activity.lower().replace(" ", "_"))
+        button = Button(button_style=ButtonStyle.blue, label=activity, custom_id=activity.lower().replace(" ", "_"))
         buttons_row2.append(button)
 
     return [buttons_row1, buttons_row2]
@@ -52,5 +52,3 @@ async def on_button_click(interaction):
     await interaction.respond(type=6)
 
 bot.run(TOKEN)
-
-
