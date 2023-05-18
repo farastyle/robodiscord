52f0662357a8f1a80228ddfe80375840a8fc482c
Rafael <rafael.oliveira@deltapoint.com.br>
2023-05-17 21:35:24 -0300

    new update gpt

--- getLastMessage.js
+++ getLastMessage.js
@@ -1,44 +1,14 @@
-const { Client, Intents } = require('discord.js');
-const channelName = 'mecsbot';  // // Set your default channel ID here
-const defaultLastMessageId = '1108528121185046618'; // Set your default message ID here
-
-client.on('ready', async () => {
-  console.log('O bot está online!');
-
-  const channel = client.channels.cache.find(channel => channel.name === channelName);
-  if (!channel) {
-    console.error(`Channel '${channelName}' not found.`);
-    return;
-  }
-
-  let lastMessageId;
-  const lastMessage = await getLastMessage(channel);
-  if (lastMessage) {
-    console.log('Found last message:', lastMessage.content);
-    lastMessageId = lastMessage.id; // Store the ID of the last message
-  } else if (defaultLastMessageId) {
-    console.log('Using default last message ID:', defaultLastMessageId);
-    lastMessageId = defaultLastMessageId; // Use the default message ID
-  } else {
-    console.error('No last message found.');
-    return;
-  }
-
-  // Export the last message ID
-  console.log('Exporting last message ID:', lastMessageId);
-  module.exports.lastMessageId = lastMessageId;
-
-  client.destroy();
-});
-
-async function getLastMessage(channel) {
-  try {
-    const messages = await channel.messages.fetch({ limit: 1 });
-    return messages.first();
-  } catch (error) {
-    console.error('Error retrieving last message:', error);
-    return null;
-  }
-}
-
-client.login(process.env.DISCORD_API_TOKEN);
+const updateMessage = async (channel, text) => {
+    let lastMessageId = channel.lastMessageId;
+    if (!lastMessageId) {
+      // Send the initial message if the last bot-generated message ID is not stored
+      const message = await channel.send({ content: text });
+      channel.lastMessageId = message.id;
+    } else {
+      let lastMessage = await channel.messages.fetch(lastMessageId);
+      await lastMessage.edit({ content: text });
+    }
+  };
+  
+  module.exports = { updateMessage };
+  
--- mecsbot.js
+++ mecsbot.js
@@ -2,15 +2,13 @@ require('dotenv').config();
 const { recordUser, removeUser } = require('./jsoncontrol');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Client, GatewayIntentBits, Partials, ButtonBuilder, ActionRowBuilder } = require('discord.js');
-const { getLastMessageId } = require('./getLastMessage');
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
+const { updateMessage } = require('./getLastMessage'); // Import the updateMessage function
 
 const token = process.env.DISCORD_API_TOKEN;
 
 let texto1 = [];
 let texto2 = [];
-let channelName = "mecsbot";
+const channelName = "mecsbot";
 
 const enterButton = new ButtonBuilder()
   .setCustomId('enter')
@@ -31,6 +29,8 @@ let text1 = texto1.length > 0 ? texto1.join('\n') : 'Ninguém';
 let text2 = texto2.length > 0 ? texto2.join('\n') : 'Nenhum registro ainda';
 let text = "```**Quem está trabalhando :**\n" + text1 + "\n**Log de chegada e saída:**\n" +  text2 + "```";
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
+
 client.on('ready', async () => {
   console.log('O bot está online!');
 
@@ -44,14 +44,9 @@ client.on('ready', async () => {
   client.channels.cache.forEach(async channel => {
     if (channel.name === channelName) {
       channel.send({ content: 'Controle de Mecs:', components: [butoes]});
-      const lastMessageId = getLastMessageId(channel.id); // Retrieve the last bot-generated message ID
-      if (lastMessageId) {
-        const lastMessage = await channel.messages.fetch(lastMessageId);
-        await lastMessage.edit({ content: text });
-      } else {
-        const message = await channel.send({ content: text });
-        setLastMessageId(channel.id, message.id); // Store the last message ID in the channel object
-      }
+      const message2 = await channel.send({ content: text });
+      console.log('ID da msg ' + message2.id);
+      channel.lastMessageId = message2.id; // Store the last message ID in the channel object
     }
   });
 });
@@ -77,20 +72,13 @@ client.on('interactionCreate', async (interaction) => {
     removeUser(user);
     texto1 = users.map(username => `${username} - ${jsonData[username]}`);
   }
-
+  
   text1 = texto1.length > 0 ? texto1.join('\n') : 'Ninguém';
   text2 = texto2.length > 0 ? texto2.join('\n') : 'Nenhum registro ainda';
   text = "``` Quem está trabalhando  : **\n" + text1 + "\n\n\n\n\n**Log de chegada e saída:**\n" +  text2 + "```";
 
-  const lastMessageId = getLastMessageId(channelId); // Retrieve the last bot-generated message ID
-  if (lastMessageId) {
-    const lastMessage = await channel.messages.fetch(lastMessageId);
-    await lastMessage.edit({ content: text });
-  } else {
-    // Send the initial message if the last bot-generated message ID is not stored
-    const message = await channel.send({ content: text });
-    setLastMessageId(channelId, message.id); // Store the last message ID in the channel object
-  }
+  // Access the updateMessage function from getLastMessage module and pass the channel and text
+  updateMessage(channel, text);
 });
 
 client.login(token);
